883523177059c728e02d0fc4cdcdb2b9585f7442
shkedya <71806271+shkedya@users.noreply.github.com>
2024-11-24 17:01:52 -0800

    Update JanggiGame.py

--- JanggiGame.py
+++ JanggiGame.py
@@ -1,5 +1,4 @@
 # Author: Alon Shkedy
-# Date: 3/10/2021
 # Description: A Janggi game that has pieces that represent a general, guard horse, elephant, chariot,
 #              cannon, and soldiers that move in different ways and can capture other pieces on a 10x9 board
 
